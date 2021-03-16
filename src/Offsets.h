4078f650cd2b8f7beff96a1e215b9ffd712e3473
Parapets <southwest85b@gmail.com>
2021-03-16 09:50:34 +0000

    Remove unused offsets

--- src/Offsets.h
+++ src/Offsets.h
@@ -2,8 +2,6 @@
 
 namespace Offset
 {
-	constexpr REL::ID CachedValues_UpdatePermanent{ 39164 };
-	constexpr REL::ID EnemyHealth_Update{ 50776 };
 	constexpr REL::ID HUDMenu_ctor{ 50854 };
 	constexpr REL::ID ActorValueMeter_Vtbl{ 551778 };
 }
