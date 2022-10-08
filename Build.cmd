b8c45d46096021011b6f5831c3e4886db13a9962
Parapets <southwest85b@gmail.com>
2022-10-08 19:35:28 +0100

    Drop VS2019 presets

--- Build.cmd
+++ Build.cmd
@@ -1,9 +1,9 @@
 @echo off
 
-cmake --preset vs2019-windows || goto :error
+cmake --preset vs2022-windows || goto :error
 cmake --build build --config Release --target PACKAGE || goto :error
 
-cmake --preset vs2019-windows-vr || goto :error
+cmake --preset vs2022-windows-vr || goto :error
 cmake --build buildVR --config Release --target PACKAGE || goto :error
 
 goto :EOF
